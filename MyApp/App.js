2c1bf38aea7c28798a7ee03f5b0c1cb3c3bfdaff
myprojectfinish <quyle5102004@gmail.com>
2024-01-15 11:38:22 +0700

    lab

---
+++ MyApp/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Lab3 from './src/screen/lab3';
+
+const App = () => {
+  return <Lab3/>;
+};
+
+export default App;
---
+++ MyApp/src/screen/lab3/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Lab3 = () => {
+  return <Text>Hello, I'm Lab3!</Text>;
+};
+
+export default Lab3;
