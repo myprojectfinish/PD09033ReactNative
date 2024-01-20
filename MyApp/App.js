e80851ca96b0aebc77cea4428834240628fb9124
myprojectfinish <quyle5102004@gmail.com>
2024-01-20 14:54:41 +0700

    Lab3

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Lab3 from './src/screen/lab3';
+import Lab33 from './src/screen/lab3.3';
 
 const App = () => {
-  return <Lab3/>;
+  return <Lab33/>;
 };
 
 export default App;
